5388122f6e791bfcb9f4a127211cc7d5a26bb35c
Jose Climaco Pereira <jose.pereira@morphis-tech.com>
2017-08-11 11:56:10 +0100

    Plugin structure

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-var plugin = function() {
-    return true;
-};
+define(function(require, exports, module) {
+    main.consumes = ["dependency"];
+    main.provides = ["c9-plugin"];
+    return main;
+
+    function main(options, imports, register) {
+        var dependency = imports.dependency;
+
+        var plugin = new Plugin("c9-plugin", main.consumes);
+
+        function doSomething(){}
+
+        plugin.pluginAPI({
+            doSomething : doSomething
+        });
+
+        register("", {
+            "c9-plugin": plugin
+        });
+    }
+});
